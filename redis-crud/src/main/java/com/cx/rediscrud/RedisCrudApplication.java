65a43760dfbae1f3f167ea269a6d9412c40fec05
zgp <zhangg.hacker@gmail.com>
2022-03-08 14:34:32 +0800

    init

---
+++ redis-crud/src/main/java/com/cx/rediscrud/RedisCrudApplication.java
@@ -0,0 +1,13 @@
+package com.cx.rediscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCrudApplication.class, args);
+    }
+
+}
---
+++ redis-crud/src/test/java/com/cx/rediscrud/RedisCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cx.rediscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
